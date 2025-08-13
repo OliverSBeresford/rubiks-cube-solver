755a6d43169b6207f6132d3098405eb79a5ed6ca
Oliver Beresford <oggynaut@icloud.com>
2025-08-12 23:41:59 -0400

    Added an import for a necessary module and created a dictionary with values needed for side rotation

--- cube.py
+++ cube.py
@@ -1,4 +1,51 @@
+import numpy as np
+
 class Cube:
+    # Adjacent rows for each face, starting from the left and going clockwise
+    # This is used to determine which rows to rotate when a face is turned
+    # For example, when turning the Up face clockwise, the Left, Back, Right,
+    # and Front faces will have their rows rotated accordingly.
+    # The slice notation is used to indicate which rows/columns are affected.
+    allItems = slice(0, None)
+    adjacent_rows = {
+        'Up': {
+            'Left': (0, allItems),
+            'Back': (0, allItems),
+            'Right': (0, allItems),
+            'Front': (0, allItems)
+        },
+        'Down': {
+            'Left': (2, allItems),
+            'Front': (2, allItems),
+            'Right': (2, allItems),
+            'Back': (2, allItems)
+        },
+        'Left': {
+            'Back': (allItems, 2),
+            'Up': (allItems, 0),
+            'Front': (allItems, 0),
+            'Down': (allItems, 0)
+        },
+        'Right': {
+            'Front': (allItems, 2),
+            'Up': (allItems, 2),
+            'Back': (allItems, 0),
+            'Down': (allItems, 2)
+        },
+        'Front': {
+            'Left': (allItems, 2),
+            'Up': (2, allItems),
+            'Right': (allItems, 0),
+            'Down': (0, allItems)
+        },
+        'Back': {
+            'Right': (allItems, 2),
+            'Up': (allItems, 2),
+            'Left': (allItems, 0),
+            'Down': (2, allItems)
+        }
+    }
+    
     def __init__(self, dictionary=None, side_length=None):
         if dictionary is None:
             # Solved rubik's Cube representation
