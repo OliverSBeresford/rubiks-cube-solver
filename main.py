2b239e4c560094c1e1d1387d576cd7c0fc07b538
Oliver Beresford <oggynaut@icloud.com>
2025-08-21 23:49:13 -0400

    Add cube turns

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from cube import Cube
 from rubiks_display import RubiksCube3DDisplay
 
 cube = Cube()
+cube.run_turns("F L F U' R U F2 L2 U' L' B D' B' L2 U")
 display = RubiksCube3DDisplay(cube)
 display.draw(elev=15, azim=-45)  # You can change angles for different views
