f990a8d46e7fd6826655bfc5861a99b95a8f213b
Oliver Beresford <oggynaut@icloud.com>
2025-08-22 21:26:44 -0400

    Improve readability

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from cube import Cube
 from rubiks_display import RubiksCube3DDisplay
 
+# Initialize a cube and the display
 cube = Cube()
-cube.run_turns("F L F U' R U F2 L2 U' L' B D' B' L2 U")
 display = RubiksCube3DDisplay(cube)
+
+# Perform a series of moves on the cube
+cube.run_turns("U B D' F2 D B' U' R2 D F2 D' R2 D F2 D' R2")
+
+# Display the cube in 3D
 display.draw(elev=15, azim=-45)  # You can change angles for different views
